5256991428bc87f5717e3855fcabe77bc45502dd
Ofaloaf <mkundakco08@charter.net>
2018-01-04 20:05:30 -0500

    Defines tweaks
    Removed defines.lua and transferred all modded entries to
    am_defines.lua, except for the military ones. They shall disappear into
    the aether instead.

--- After the End/common/defines/am_defines.lua
+++ After the End/common/defines/am_defines.lua
@@ -1,3 +1,8 @@
+NDefines.NTechnology.IDEAL_YEAR_LEVEL_0 = 2450
+NDefines.NTechnology.IDEAL_YEAR_LEVEL_8 = 3100
+
+NDefines.NEngine.MISSING_SCRIPTED_SUCCESSOR_ERROR_CUTOFF_YEAR = 2670
+
 NDefines.NFrontend.CAMERA_LOOKAT_X = 1000.0
 NDefines.NFrontend.CAMERA_LOOKAT_Y = 0.0
 NDefines.NFrontend.CAMERA_LOOKAT_Z = 900.0
@@ -45,3 +50,39 @@ NDefines.NFrontend.CREDITS_POS_Z = 1308.0
 NDefines.NFrontend.CREDITS_LOOK_X = 900.0
 NDefines.NFrontend.CREDITS_LOOK_Y = 0.0
 NDefines.NFrontend.CREDITS_LOOK_Z = 1450.0
+
+NDefines.NEndGame.DYN1_SCORE = 100000
+NDefines.NEndGame.DYN1_ID = 1851041
+NDefines.NEndGame.DYN2_SCORE = 90000
+NDefines.NEndGame.DYN2_ID = 1782000
+NDefines.NEndGame.DYN3_SCORE = 80000
+NDefines.NEndGame.DYN3_ID = 1776013
+NDefines.NEndGame.DYN4_SCORE = 70000
+NDefines.NEndGame.DYN4_ID = 1779000
+NDefines.NEndGame.DYN5_SCORE = 60000
+NDefines.NEndGame.DYN5_ID = 1795000
+NDefines.NEndGame.DYN6_SCORE = 50000
+NDefines.NEndGame.DYN6_ID = 1491003
+NDefines.NEndGame.DYN7_SCORE = 40000
+NDefines.NEndGame.DYN7_ID = 1650002
+NDefines.NEndGame.DYN8_SCORE = 30000
+NDefines.NEndGame.DYN8_ID = 1789000
+NDefines.NEndGame.DYN9_SCORE = 20000
+NDefines.NEndGame.DYN9_ID = 1889009
+NDefines.NEndGame.DYN10_SCORE = 15000
+NDefines.NEndGame.DYN10_ID = 1519000
+NDefines.NEndGame.DYN11_SCORE = 10000
+NDefines.NEndGame.DYN11_ID = 100239
+NDefines.NEndGame.DYN12_SCORE = 7500
+NDefines.NEndGame.DYN12_ID = 261
+NDefines.NEndGame.DYN13_SCORE = 5000
+NDefines.NEndGame.DYN13_ID = 1788046
+NDefines.NEndGame.DYN14_SCORE = 2000
+NDefines.NEndGame.DYN14_ID = 1796003
+NDefines.NEndGame.DYN15_SCORE = 1000
+NDefines.NEndGame.DYN15_ID = 1790001
+
+NDefines.NLearningScenario.START_YEAR = 2666
+NDefines.NLearningScenario.START_MONTH = 7
+NDefines.NLearningScenario.START_DAY = 4
+NDefines.NLearningScenario.START_CHARACTER = 1788001
